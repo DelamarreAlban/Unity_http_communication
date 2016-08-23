e107607a75bb491bbb4b89a6cd894089d2e3d9c5
DelamarreAlban <alban.delamarre@gmail.com>
2016-08-23 08:33:05 -0400

    Mascaret integration
    Mascaret integration finished
    Capsule has a new name, aka Ralph

--- Unity_http_communication_sample/Assets/Behaviors/Capsule_Agent_Move.cs
+++ Unity_http_communication_sample/Assets/Behaviors/Capsule_Agent_Move.cs
@@ -9,30 +9,30 @@ public class Capsule_Agent_Move : BehaviorExecution  {
 	
 	public int state = 0;
 	public GameObject target;
-	public GameObject agent;
+	public GameObject entity;
 	public string entityName = "";
-
+    NavMeshAgent agent;
 	
 	public Capsule_Agent_Move ()
 	{
-		
-	}
+    }
 	
 	public override void init(Behavior specif,InstanceSpecification host,Dictionary<string,ValueSpecification> p,bool sync)
 	{
 		base.init(specif, host, p,sync);
 		entityName = Host.name;
-		Debug.Log ("Name :     " + entityName);
-		agent = GameObject.Find (entityName);
-		target = GameObject.Find ("Target");
+		Debug.Log ("Name : " + entityName);
+        entity = GameObject.Find (entityName);
+        agent = entity.GetComponent<NavMeshAgent>();
+        target = GameObject.Find ("Target");
 	}
 	
 	override public double execute (double dt)
 	{
 		if (state == 0) {
 			Debug.Log ("===================================MOVE==========================================       " + entityName);
-			agent.transform.position = target.transform.position;
-			//agent.SetDestination (target.position);
+			//agent.transform.position = target.transform.position;
+			agent.SetDestination (target.transform.position);
 			state ++;
 			return 1.0;
 
